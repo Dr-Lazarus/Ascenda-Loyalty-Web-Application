60b997995829ed649ad77e3636e455b0d8726221
Dhanushkmr <Dhanush5247@gmail.com>
2022-07-06 21:01:20 +0800

    new design first commit

--- src/App.js
+++ src/App.js
@@ -1,38 +1,33 @@
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Home from "./components/Home/Home";
-import Login from "./components/Login/Login";
-import NavBar from "./components/NavBar/NavBar"; 
-import LoginForm from "./components/Login/LoginForm";
-import Register from "./components/Register/Register";
-import "./App.css"
-// import "./index.css"
-import {Link} from 'react-router-dom';
+import React from 'react';
+import './App.css';
+import Home from './Home'
+import Header from './Header'
+import Footer from './Footer'
+import SearchPage from './SearchPage'
 
-export default function App() {
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
   return (
-    <BrowserRouter>
-    <div className="App">
-      <NavBar/>
-      <div className="content">
-      <Switch>
-        <Route exact path="/">
-          <Home/>
-        </Route>
 
-        <Route path="/login">
-          <LoginForm className="login-component"/>
-        </Route>
-
-        <Route path="/register">
-          <Register className="register-component"/>
-        </Route>
-      </Switch>
-      </div>
+    // BEM
+    <div className="app">
+      <Router>
+        <Header />
+        
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        
+        <Footer />
+      </Router>
     </div>
-    </BrowserRouter>
   );
 }
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(<App/>);
+export default App;
--- src/components/Login/Login.js
+++
@@ -1,9 +0,0 @@
-function Login(){
-    return(
-        <div>
-            <h2>This is suppose to be a login form.</h2>
-        </div>
-    )
-}
-
-export default Login;
