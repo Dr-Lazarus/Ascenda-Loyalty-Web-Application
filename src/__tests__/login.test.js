839c991ff2f16a01fcc108453c56d8401712a545
oakar-00 <oakkarrr00@gmail.com>
2022-07-20 00:07:30 +0800

    testing getting setup

--- src/__tests__/login.test.js
+++ src/__tests__/login.test.js
@@ -1,13 +1,29 @@
 import { render, screen, cleanup } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
+import renderer from "react-test-renderer";
+import { Renderer } from "react-dom";
 import Login from "./../Login";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter, MemoryRouter } from "react-router-dom";
 import App from "./../App";
 import React from "react";
 
-test("renders react component", async () => {
-	render(
-		<React.StrictMode>
-			<App />,
-		</React.StrictMode>
-	);
+afterEach(() => {
+	cleanup();
+});
+
+const renderWithRouter = (ui, { route = "/" } = {}) => {
+	window.history.pushState({}, "Test page", route);
+
+	return {
+		user: userEvent.setup(),
+		...render(ui, { wrapper: BrowserRouter }),
+	};
+};
+
+test("renders login component", async () => {
+	const loginRoute = "/login";
+	renderWithRouter(<Login />, { loginRoute });
+
+	expect(screen.getByTestId("login-1")).toHaveTextContent("Login");
 });
