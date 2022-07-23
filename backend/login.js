e86ebee32a8940f7f3778a26326c6e4a9e70b7ea
poopysitoh <bryansitoh@gmail.com>
2022-07-23 13:19:45 +0800

    changed userschema

--- backend/login.js
+++ backend/login.js
@@ -37,7 +37,33 @@ button.addEventListener("click", () => {
     });
 });
 
-export const login = async (email, password) => {
+export const register = async (email, password) => {
+    try {
+      const res = await axios({
+        method: 'POST',
+        url: 'http://localhost:5000/api/users/register',
+        data: {
+          email,
+          password,
+          firstName ,
+          lastName,
+          confirmPassword,
+          contactNumber,
+        }
+      });
+  
+      if (res.data.status === 'success') {
+        showAlert('success', 'Logged in successfully!');
+        window.setTimeout(() => {
+          location.assign('/profile');
+        }, 1500);
+      }
+    } catch (err) {
+      showAlert('error', err.response.data.message);
+    }
+  };
+
+  export const login = async (email, password) => {
     try {
       const res = await axios({
         method: 'POST',
