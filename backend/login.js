50db6db8d3ed4f4fa75040a8f38abea089984152
poopysitoh <bryansitoh@gmail.com>
2022-07-23 12:36:08 +0800

    frontend backend setup

---
+++ backend/login.js
@@ -0,0 +1,60 @@
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+const button = document.getElementById("btn");
+
+button.addEventListener("click", () => {
+  const email = emailInput.value;
+  const password = passwordInput.value;
+
+  axios.post("http://localhost:5000/api/users", {
+      email: email,
+      password: password
+    })
+    .then((response) => {
+      console.log(response);
+    });
+});
+const nameInput = document.getElementById("name");
+const picInput = document.getElementById("pic");
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+const button = document.getElementById("btn");
+
+button.addEventListener("click", () => {
+  const email = emailInput.value;
+  const password = passwordInput.value;
+  const name = nameInput.value;
+  const pic = picInput.value;
+
+  axios.post("http://localhost:5000/api/register", {
+      email: email,
+      password: password,
+      name : name,
+      pic : pic
+    })
+    .then((response) => {
+      console.log(response);
+    });
+});
+
+export const login = async (email, password) => {
+    try {
+      const res = await axios({
+        method: 'POST',
+        url: 'http://localhost:5000/api/users/login',
+        data: {
+          email,
+          password
+        }
+      });
+  
+      if (res.data.status === 'success') {
+        showAlert('success', 'Logged in successfully!');
+        window.setTimeout(() => {
+          location.assign('/profile');
+        }, 1500);
+      }
+    } catch (err) {
+      showAlert('error', err.response.data.message);
+    }
+  };
---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+import data from "./data.js";
+import userRoutes from "./userRoutes.js";
+import connectDB from "./mongooseconfig/db.js"
+
+dotenv.config();
+connectDB();
+const app = express()
+
+app.use(express.urlencoded({extended : false}))
+app.use(express.json());
+app.use("/api/users", userRoutes);
+// app.get("/", (req,res) => {
+//     res.send("API RUN CHECK")
+// })
+// app.get("/api/users", (req,res) => {
+//     res.json(data) })
+app.listen(5000, console.log(`Server started `))
