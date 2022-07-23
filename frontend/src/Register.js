6e8ffd6d75e85158334e6184fad816a8d53b1e6a
Dhanushkmr <Dhanush5247@gmail.com>
2022-07-23 19:13:53 +0800

    register backend integration

--- frontend/src/Register.js
+++ frontend/src/Register.js
@@ -1,8 +1,38 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Button } from "@material-tailwind/react";
+import axios from "axios";
 
 function Register() {
+	const navigate = useNavigate();
+	const [email, setEmail] = useState("raw@wwe.com");
+	const [password, setPassword] = useState("");
+	const [firstname, setfirstname] = useState("");
+	const [lastname, setlastname] = useState("");
+	const [contact, setContact] = useState("");
+
+	const submitHandler = (e) => {
+		axios({
+			method: "POST",
+			url: "http://localhost:5001/api/users/register",
+			data: {
+				email: email,
+				password: password,
+				name: firstname,
+				pic: "pic",
+			},
+		})
+			.then(function (response) {
+				if (response.statusText === "OK") {
+					console.log("success", "Registered successfully!");
+					navigate("/profile");
+				}
+				console.log(response);
+			})
+			.catch(function (error) {
+				console.log("error", error.response.data.message);
+			});
+	};
 	return (
 		<div className="flex justify-center p-20">
 			<form className="">
@@ -18,6 +48,7 @@ function Register() {
 								type="firstName"
 								name="firstName"
 								id="firstName"
+								onChange={(e) => setfirstname(e.target.value)}
 							/>
 						</div>
 
@@ -28,6 +59,7 @@ function Register() {
 								type="lastName"
 								name="lastName"
 								id="lastName"
+								onChange={(e) => setlastname(e.target.value)}
 							/>
 						</div>
 
@@ -38,6 +70,7 @@ function Register() {
 								type="contact"
 								name="contact"
 								id="contact"
+								onChange={(e) => setContact(e.target.value)}
 							/>
 						</div>
 					</div>
@@ -50,6 +83,7 @@ function Register() {
 								type="email"
 								name="email"
 								id="email"
+								onChange={(e) => setEmail(e.target.value)}
 							/>
 						</div>
 
@@ -60,6 +94,7 @@ function Register() {
 								type="password"
 								name="password"
 								id="password"
+								onChange={(e) => setPassword(e.target.value)}
 							/>
 						</div>
 
@@ -72,7 +107,9 @@ function Register() {
 								id="repassword"
 							/>
 						</div>
-						<Button className="mt-5 w-52">REGISTER</Button>
+						<Button className="mt-5 w-52" onClick={submitHandler}>
+							REGISTER
+						</Button>
 					</div>
 				</div>
 			</form>
